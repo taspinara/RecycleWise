1d74b5df5457e21e7e0d2a8b5e68a57ccac22f9a
Ali <taspinara17@gmail.com>
2025-04-23 19:17:49 +0200

    first commit in dev branch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,7 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl bg-amber-400 text-center">
-        Hello world
-      </h1>
-      <button className="btn btn-primary">Default</button>
+      
     </>
   )
 }
