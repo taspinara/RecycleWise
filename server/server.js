a255ccf827f68fad4df8bc5e5804d689dcf77c42
Ali <taspinara17@gmail.com>
2025-04-23 19:07:06 +0200

    first commit

---
+++ server/server.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+import connectDB from './config/db.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+const MODE = process.env.MODE;
+
+app.use(cors({
+  origin: process.env.CLIENT_ORIGIN, // Frontend URL
+  credentials: true               // ✅ Allow cookies to be sent with requests
+}));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
+
+// Import Routes
+
+
+app.get(/.*/, (req, res) => {
+  res.status(404).send('Page does not exist');
+});
+
+// Server start
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`🚀 Server is running in ${MODE} mode on ${PORT}`);
+  });
+});
