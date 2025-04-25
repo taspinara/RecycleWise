de113020fe763f78d07e1d01c32bf3765a0bc680
dahomeykid <dahomeykid@gmail.com>
2025-04-25 12:18:41 +0200

    added Context and Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import { RecycleWiseProvider } from './context/RecycleWiseContext'
 
 function App() {
 
   return (
-    <>
-     <Home /> 
-    </>
+    <Router>
+      <RecycleWiseProvider>
+        <Routes>
+          <Route index element={<Home />} />
+          {/* <Route path="/service" element={<Service />} /> */}
+          {/* <Route path="/admin" element={<Admin />} /> */}
+          {/* <Route path="/quiz" element={<Quiz />} /> */}
+          {/* Add more routes as needed */}
+        </Routes>
+      </RecycleWiseProvider>
+    </Router>
   )
 }
 
