ef4c54d267a7551bb4772e8c82c0fae7265f7d5c
Bleon5--Github--Username <bleonarllati2@gmail.com>
2025-05-07 15:18:05 -0400

    Speedometer feature added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 import {
-	BrowserRouter as Router,
-	Routes,
-	Route,
-	Navigate,
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
 } from "react-router-dom";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
@@ -15,105 +15,71 @@ import Register from "./components/Register";
 import AuthenticatedUser from "./components/AuthenticatedUser";
 
 import {
-	useRecycleWise,
-	RecycleWiseProvider,
+  useRecycleWise,
+  RecycleWiseProvider,
 } from "./context/RecycleWiseContext";
 import Chatbot from "./components/Chatbot/Chatbot";
+import SpeedometerDashboard from "./components/Speedometer";
 
 const ProtectedRoute = ({ children }) => {
-	const { isAuthenticated } = useRecycleWise();
-	if (!isAuthenticated) {
-		return (
-			<Navigate
-				to='/login'
-				replace
-			/>
-		);
-	}
+  const { isAuthenticated } = useRecycleWise();
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
 
-	return children;
+  return children;
 };
 
 // Admin route to be protected
 const AdminRoute = ({ children }) => {
-	const { isAdmin } = useRecycleWise();
+  const { isAdmin } = useRecycleWise();
 
-	const { isAuthenticated } = useRecycleWise();
+  const { isAuthenticated } = useRecycleWise();
 
-	if (!isAuthenticated) {
-		return (
-			<Navigate
-				to='/login'
-				replace
-			/>
-		);
-	}
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
 
-	if (!isAdmin) {
-		return (
-			<Navigate
-				to='/login'
-				replace
-			/>
-		);
-	}
+  if (!isAdmin) {
+    return <Navigate to="/login" replace />;
+  }
 
-	return children;
+  return children;
 };
 
 function App() {
-	return (
-		<div className='flex flex-col min-h-screen'>
-			<Router>
-				<RecycleWiseProvider>
-					<Navbar />
-					<div className='flex-grow'>
-						<Routes>
-							<Route
-								index
-								element={<Home />}
-							/>
-							<Route
-								path='/events'
-								element={<Events />}
-							/>
-							<Route
-								path='/admin'
-								element={
-									<AdminRoute>
-										<Admin />
-									</AdminRoute>
-								}
-							/>
-							<Route
-								path='/leaderboard'
-								element={<LeaderBoard />}
-							/>
-							<Route
-								path='/login'
-								element={<Login />}
-							/>
-							<Route
-								path='/register'
-								element={<Register />}
-							/>
-							<Route
-								path='/recycling-tips'
-								element={<LeaderBoard />}
-							/>
-							<Route
-								path='/me'
-								element={<AuthenticatedUser />}
-							/>
-							{/* Add more routes as needed */}
-						</Routes>
-					</div>
-					<Footer />
-					<Chatbot />
-				</RecycleWiseProvider>
-			</Router>
-		</div>
-	);
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Router>
+        <RecycleWiseProvider>
+          <Navbar />
+          <div className="flex-grow">
+            <Routes>
+              <Route index element={<Home />} />
+              <Route path="/events" element={<Events />} />
+              <Route
+                path="/admin"
+                element={
+                  <AdminRoute>
+                    <Admin />
+                  </AdminRoute>
+                }
+              />
+              <Route path="/leaderboard" element={<LeaderBoard />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/recycling-tips" element={<LeaderBoard />} />
+              <Route path="/me" element={<AuthenticatedUser />} />
+              {/* Add more routes as needed */}
+            </Routes>
+          </div>
+          <SpeedometerDashboard />
+          <Footer />
+          <Chatbot />
+        </RecycleWiseProvider>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
